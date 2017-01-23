7e1d080676e4d99963e0f2b3bba3866002a63471
박민현 <algusl92@naver.com>
2017-01-23 14:38:39 +0900

    EmguCV 및 AForge 참조 추가

--- Eyeblink_EmguCV/Eyeblink_EmguCV/Form1.Designer.cs
+++ Eyeblink_EmguCV/Eyeblink_EmguCV/Form1.Designer.cs
@@ -29,11 +29,134 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.imageBoxCapturedFrame = new Emgu.CV.UI.ImageBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label2 = new System.Windows.Forms.Label();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.label3 = new System.Windows.Forms.Label();
+            this.imageBox2 = new Emgu.CV.UI.ImageBox();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxCapturedFrame)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBoxCapturedFrame
+            // 
+            this.imageBoxCapturedFrame.Location = new System.Drawing.Point(-1, -1);
+            this.imageBoxCapturedFrame.Name = "imageBoxCapturedFrame";
+            this.imageBoxCapturedFrame.Size = new System.Drawing.Size(412, 393);
+            this.imageBoxCapturedFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBoxCapturedFrame.TabIndex = 3;
+            this.imageBoxCapturedFrame.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(212, 434);
+            this.trackBar1.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 20;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(73, 45);
+            this.trackBar1.TabIndex = 16;
+            this.trackBar1.Value = 20;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(289, 443);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 12);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "label2";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.LargeChange = 2;
+            this.trackBar2.Location = new System.Drawing.Point(338, 434);
+            this.trackBar2.Margin = new System.Windows.Forms.Padding(2);
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.Size = new System.Drawing.Size(73, 45);
+            this.trackBar2.TabIndex = 18;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(443, 434);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 12);
+            this.label3.TabIndex = 19;
+            this.label3.Text = "label3";
+            // 
+            // imageBox2
+            // 
+            this.imageBox2.Location = new System.Drawing.Point(504, 431);
+            this.imageBox2.Name = "imageBox2";
+            this.imageBox2.Size = new System.Drawing.Size(48, 48);
+            this.imageBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox2.TabIndex = 20;
+            this.imageBox2.TabStop = false;
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(445, 182);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(100, 100);
+            this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.imageBox1.TabIndex = 21;
+            this.imageBox1.TabStop = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(582, 431);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 22;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 519);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.imageBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.imageBoxCapturedFrame);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBoxCapturedFrame)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Emgu.CV.UI.ImageBox imageBoxCapturedFrame;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.Label label3;
+        private Emgu.CV.UI.ImageBox imageBox2;
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
 
--- Eyeblink_EmguCV/Eyeblink_EmguCV/Form1.cs
+++ Eyeblink_EmguCV/Eyeblink_EmguCV/Form1.cs
@@ -8,13 +8,107 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.VideoSurveillance;
+using System.Threading;
+
+using AForge;
+using AForge.Imaging;
+using AForge.Imaging.Filters;
+
 namespace Eyeblink_EmguCV
 {
     public partial class Form1 : Form
     {
+        private Capture _capture;
+        private HaarCascade _faces;
+        private Image<Bgr, Byte> frame;
+
+        private Rectangle possibleROI_rightEye, possibleROI_leftEye;
+        private LineSegment2D a, b, c;
+        private Bgr d;
+
+        private Bitmap newBitmap;
+        private int blurAmount = 1;
+        int cannyValueX = 1;
+        int cannyValueY = 1;
+
         public Form1()
         {
             InitializeComponent();
+
+            // capture 생성
+            if (_capture == null)
+            {
+                try
+                {
+                    _capture = new Capture();
+                    _faces = new HaarCascade("C:\\haarcascade_frontalface_alt_tree.xml");
+                }
+                catch (NullReferenceException excpt) { }
+            }
+
+            if (_capture != null)
+            {
+                Application.Idle += FrameGrabber;
+            }
         }
+
+
+        void FrameGrabber(object sender, EventArgs e)
+        {
+            //새로운 Frame 얻기
+            frame = _capture.QueryFrame();
+            imageBoxCapturedFrame.Image = frame;
+
+            //grayscale로 변환( haarcascade를 적용할 땐 회색 화면을 더 잘 잡는다고 함 )
+            Image<Gray, Byte> grayFrame = frame.Convert<Gray, Byte>();
+            grayFrame._EqualizeHist();
+
+            // 머신러닝을 이용한 얼굴 인식 Haaracascade 돌리기
+            MCvAvgComp[][] facesDetected = grayFrame.DetectHaarCascade(_faces, 1.1, 0, Emgu.CV.CvEnum.HAAR_DETECTION_TYPE.FIND_BIGGEST_OBJECT, new Size(20, 20));
+            if (facesDetected[0].Length != 0)
+            {
+                MCvAvgComp face = facesDetected[0][0];
+
+                #region 얼굴 인식한것을 토대로 눈 찾기
+                Int32 yCoordStartSearchEyes = face.rect.Top + (face.rect.Height * 3 / 11);
+                System.Drawing.Point startingPointSearchEyes = new System.Drawing.Point(face.rect.X, yCoordStartSearchEyes);
+                System.Drawing.Point endingPointSearchEyes = new System.Drawing.Point((face.rect.X + face.rect.Width), yCoordStartSearchEyes);
+
+                Size searchEyesAreaSize = new Size(face.rect.Width, (face.rect.Height * 2 / 9));
+                Size eyeAreaSize = new Size(face.rect.Width / 2, (face.rect.Height * 2 / 9));
+                System.Drawing.Point lowerEyesPointOptimized = new System.Drawing.Point(face.rect.X, yCoordStartSearchEyes + searchEyesAreaSize.Height);
+                System.Drawing.Point startingLeftEyePointOptimized = new System.Drawing.Point(face.rect.X + face.rect.Width / 2, yCoordStartSearchEyes);
+
+                Rectangle leftEyeArea = new Rectangle(new System.Drawing.Point(startingPointSearchEyes.X + 25, startingPointSearchEyes.Y + 15),
+                                                     new Size(eyeAreaSize.Width - 25, eyeAreaSize.Height - 25));
+                Rectangle rightEyeArea = new Rectangle(new System.Drawing.Point(startingLeftEyePointOptimized.X + 5, startingLeftEyePointOptimized.Y + 15),
+                                                     new Size(eyeAreaSize.Width - 33, eyeAreaSize.Height - 25));
+                #endregion
+
+                #region 눈 영역 그리기
+                a = new LineSegment2D(startingPointSearchEyes, endingPointSearchEyes);
+                b = new LineSegment2D(new System.Drawing.Point(lowerEyesPointOptimized.X, lowerEyesPointOptimized.Y),
+                                      new System.Drawing.Point((lowerEyesPointOptimized.X + face.rect.Width), (yCoordStartSearchEyes + searchEyesAreaSize.Height)));
+                d = new Bgr(Color.Chocolate);
+
+                //그리기
+                frame.Draw(a, d, 3);
+                frame.Draw(b, d, 3);
+                #endregion
+
+                #region 눈 영역 검출한 Rectangle의 크기가 양수일 경우에만 눈 영역 적출하기
+                if (leftEyeArea.Width > 0 && leftEyeArea.Height > 0 && rightEyeArea.Width > 0 && rightEyeArea.Height > 0)
+                {
+                    possibleROI_leftEye = leftEyeArea;
+                    possibleROI_rightEye = rightEyeArea;
+                }
+                #endregion 
+            }// if(faceDetect[0])
+        }
+        
     }
 }
