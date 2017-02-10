a6fddf015c0e0461786ad4dd1328a8ae7d3c967e
박민현 <algusl92@naver.com>
2017-02-10 20:49:14 +0900

    Face Training을 위한 EigenObjectRecognizer.class 추가

--- Eyeblink_EmguCV/Eyeblink_EmguCV/Form1.Designer.cs
+++ Eyeblink_EmguCV/Eyeblink_EmguCV/Form1.Designer.cs
@@ -44,8 +44,9 @@
             // 
             this.imageBoxCapturedFrame.Dock = System.Windows.Forms.DockStyle.Fill;
             this.imageBoxCapturedFrame.Location = new System.Drawing.Point(0, 0);
+            this.imageBoxCapturedFrame.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.imageBoxCapturedFrame.Name = "imageBoxCapturedFrame";
-            this.imageBoxCapturedFrame.Size = new System.Drawing.Size(664, 519);
+            this.imageBoxCapturedFrame.Size = new System.Drawing.Size(949, 778);
             this.imageBoxCapturedFrame.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imageBoxCapturedFrame.TabIndex = 3;
             this.imageBoxCapturedFrame.TabStop = false;
@@ -56,8 +57,7 @@
             this.label2.BackColor = System.Drawing.Color.Black;
             this.label2.Font = new System.Drawing.Font("넥슨 풋볼고딕 B", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label2.ForeColor = System.Drawing.Color.Red;
-            this.label2.Location = new System.Drawing.Point(504, 196);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(720, 294);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(169, 30);
             this.label2.TabIndex = 17;
@@ -69,8 +69,7 @@
             this.label3.BackColor = System.Drawing.Color.Black;
             this.label3.Font = new System.Drawing.Font("넥슨 풋볼고딕 B", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
             this.label3.ForeColor = System.Drawing.Color.Red;
-            this.label3.Location = new System.Drawing.Point(571, 235);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(816, 352);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(28, 30);
             this.label3.TabIndex = 19;
@@ -78,18 +77,20 @@
             // 
             // imageBox1
             // 
-            this.imageBox1.Location = new System.Drawing.Point(534, 382);
+            this.imageBox1.Location = new System.Drawing.Point(763, 573);
+            this.imageBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.imageBox1.Name = "imageBox1";
-            this.imageBox1.Size = new System.Drawing.Size(130, 137);
+            this.imageBox1.Size = new System.Drawing.Size(186, 206);
             this.imageBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imageBox1.TabIndex = 21;
             this.imageBox1.TabStop = false;
             // 
             // imageBox3
             // 
-            this.imageBox3.Location = new System.Drawing.Point(534, 0);
+            this.imageBox3.Location = new System.Drawing.Point(763, 0);
+            this.imageBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.imageBox3.Name = "imageBox3";
-            this.imageBox3.Size = new System.Drawing.Size(130, 137);
+            this.imageBox3.Size = new System.Drawing.Size(186, 206);
             this.imageBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.imageBox3.TabIndex = 23;
             this.imageBox3.TabStop = false;
@@ -97,23 +98,25 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(507, 286);
+            this.label1.Location = new System.Drawing.Point(724, 429);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 12);
+            this.label1.Size = new System.Drawing.Size(54, 18);
             this.label1.TabIndex = 24;
             this.label1.Text = "label1";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(664, 519);
+            this.ClientSize = new System.Drawing.Size(949, 778);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.imageBox3);
             this.Controls.Add(this.imageBox1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.imageBoxCapturedFrame);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.imageBoxCapturedFrame)).EndInit();
